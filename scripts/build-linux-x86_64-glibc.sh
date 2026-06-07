cc35b936dd0adf1a8b2205899faac9e7adc25171
huaimi123 <131643111+huaimi123@users.noreply.github.com>
2026-06-07 11:42:31 +0800

    Delete scripts directory

--- scripts/build-linux-x86_64-glibc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-export TARGET_PLATFORM=linux-x86_64-glibc
-
-exec "${SCRIPT_DIR}/build-native-plugin-binaries-common.sh" "$@"
